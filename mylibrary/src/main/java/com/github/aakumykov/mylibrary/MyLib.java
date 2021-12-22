85c501ded4d107914cddcc608e61f09d97c756b4
Andrey Kumykov <aakumykov@yandex.ru>
2021-12-22 12:42:07 +0700

    Метод doWork().

--- mylibrary/src/main/java/com/github/aakumykov/mylibrary/MyLib.java
+++ mylibrary/src/main/java/com/github/aakumykov/mylibrary/MyLib.java
@@ -1,4 +1,10 @@
 package com.github.aakumykov.mylibrary;
 
+import android.util.Log;
+
 public class MyLib {
+
+    public void doWork() {
+        Log.d(":-)", "Class "+MyLib.class.getSimpleName()+" doing work");
+    }
 }
